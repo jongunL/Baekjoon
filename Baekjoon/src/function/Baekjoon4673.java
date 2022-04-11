ff495b7834743017ff79b28f4ab4fb44805397bc
jongunL <98376876+jongunL@users.noreply.github.com>
2022-04-11 23:40:39 +0900

    solve

--- Baekjoon/src/function/Baekjoon4673.java
+++ Baekjoon/src/function/Baekjoon4673.java
@@ -24,6 +24,28 @@ public class Baekjoon4673 {
 		
 		 */
 
+		boolean[] check = new boolean[10001];
+		
+		for(int i=1; i<10001; i++) {
+			int n = m1(i);
+			if(n<10001) check[n] = true;
+		}
+		for(int i=1; i<10001; i++) {
+			if(!check[i]) System.out.println(i);
+		}
+		
 	}
+	public static int m1(int number) {
+		int sum = number;
+		
+		while(number != 0) {
+			sum = sum + (number %10);
+			number = number/10;
+		}
+		
+		return sum;
+	}
+
+
 
 }
